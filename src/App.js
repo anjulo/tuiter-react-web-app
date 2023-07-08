9b63181c43b89a28746fc81362f64af1cb10c750
bereket <iberekety@gmail.com>
2023-07-08 13:35:11 -0400

    Revert "Apply React"
    This reverts commit c645fcef6afc642828d74610a810d54523bfd930.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
-import React from 'react';
-import Labs from './labs'
-import HelloWorld from './labs/a6/hello-world';
-import Tuiter from './tuiter';
-import { BrowserRouter } from 'react-router-dom';
-import { Routes, Route } from "react-router"
-
 function App() {
   return (
-    <BrowserRouter>
-      <div className="container">
-        <Routes>
-          {/* <Route path='/hello' element={<HelloWorld />} /> */}
-          <Route index element={<HelloWorld />} />
-          <Route path='/labs' element={<Labs />} />
-          <Route path='/tuiter/*' element={<Tuiter />}/>
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+    </div>
   );
 }
 
