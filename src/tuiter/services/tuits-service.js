638b5d813b5f7e8d4ddaf16f0fd877fb543b1809
bereket <iberekety@gmail.com>
2023-08-03 22:24:33 -0400

    Add services

---
+++ src/tuiter/services/tuits-service.js
@@ -0,0 +1,15 @@
+import axios from 'axios';  // import axios
+const TUITS_API = 'http://localhost:4000/api/tuits';  // location of HTTP services
+
+
+export const createTuit = async (tuit) => {}
+
+export const findTuits = async () => {            // async tags this function as asynchronous
+  const response = await axios.get(TUITS_API);    // send HTTP GET request to TUITS_API
+  const tuits = response.data;                    // extract JSON array from response from server 
+  return tuits;                                  // return tuits            
+ }
+ 
+export const deleteTuit = async (tuit) => {}
+export const updateTuit = async (tuit) => {}
+
---
+++ src/tuiter/services/tuits-thunks.js
@@ -0,0 +1,8 @@
+import { createAsyncThunk } from "@reduxjs/toolkit"   // import createAsyncTrunk
+import * as service from "./tuits-service"            // import all exported functions as service
+
+
+export const findTuitsThunk = createAsyncThunk(      // create thunk for findTuits
+  'tuits/findTuits', async () =>                     // give unique name, thunk invokes
+  await service.findTuits()                         // service function. Returned data goes in        
+)                                                   // redux action's payload
