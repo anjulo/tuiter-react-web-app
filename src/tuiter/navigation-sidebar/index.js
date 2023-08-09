e75345334cd45694bdd669222a2384cb5e629c1e
bereket <iberekety@gmail.com>
2023-08-09 19:20:49 -0400

    Refactor Navigation side bar using looping

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -6,63 +6,21 @@ import { Link } from "react-router-dom";
 const NavigationSidebar = () => {
   const { pathname } = useLocation();
   const parts = pathname.split('/');
+  const screens = ["home", "explore", "notifications", "messages", "bookmarks", "lists", "profile", "register", "login", "more"]
+  const icons = ["fa-home", "fa-hashtag", "fa-bell", "fa-envelope", "fa-bookmark", "fa-list", "fa-user", "fa-user-plus", "fa-right-to-bracket", "fa-ellipsis-h"]
+
   return (
     <div className="list-group">
       <Link to="/tuiter" className="list-group-item"><i className="fab fa-twitter me-1 fa-2x"></i></Link>
-      <Link to="/tuiter/home" className={`list-group-item ${parts.at(-1) === "" || parts.at(-1) === "home" ? 'active' : ''}`}>
-        <div className="row">
-          <div className="col-12 col-xl-2 col-xxl-2"><i className="fas fa-home me-1"></i> </div>
-          <div className="d-none d-xl-block col-10">Home</div>
-        </div>
-      </Link>
-      <Link to="/tuiter/explore" className={`list-group-item ${parts.at(-1) === 'explore' ? 'active' : ''}`}>
-        <div className="row">
-          <div className="col-12 col-xl-2 col-xxl-2"><i className="fas fa-hashtag me-1"></i> </div>
-          <div className="d-none d-xl-block col-10">Explore</div>
-        </div>
-      </Link>
-      <Link to="/tuiter/notifications" className={`list-group-item ${parts.at(-1) === 'notifications' ? 'active' : ''}`}>
-        <div className="row">
-          <div className="col-12 col-xl-2 col-xxl-2"><i className="fas fa-bell me-1"></i> </div>
-          <div className="d-none d-xl-block col-10">Notifications</div>
-        </div>
-      </Link>
-      <Link to="/tuiter/messages" className={`list-group-item ${parts.at(-1) === 'messages' ? 'active' : ''}`}>
-        <div className="row">
-          <div className="col-12 col-xl-2 col-xxl-2"><i className="fas fa-envelope me-1"></i> </div>
-          <div className="d-none d-xl-block col-10">Messages</div>
-        </div>
-      </Link>
-      <Link to="/tuiter/bookmarks" className={`list-group-item ${parts.at(-1) === 'bookmarks' ? 'active' : ''}`}>
-        <div className="row">
-          <div className="col-12 col-xl-2 col-xxl-2"><i className="fas fa-bookmark me-1"></i> </div>
-          <div className="d-none d-xl-block col-10">Bookmarks</div>
-        </div>
-      </Link>
-      <Link to="/tuiter/lists" className={`list-group-item ${parts.at(-1) === 'lists' ? 'active' : ''}`}>
-        <div className="row">
-          <div className="col-12 col-xl-2 col-xxl-2"><i className="fas fa-list me-1"></i> </div>
-          <div className="d-none d-xl-block col-10">Lists</div>
-        </div>
-      </Link>
-      <Link to="/tuiter/profile" className={`list-group-item ${parts.at(-1) === 'profile' ? 'active' : ''}`}>
-        <div className="row">
-          <div className="col-12 col-xl-2 col-xxl-2"><i className="fas fa-user me-1"></i> </div>
-          <div className="d-none d-xl-block col-10">Profile</div>
-        </div>
-      </Link>
-      <Link to="tuiter/more" className={`list-group-item ${parts.at(-1) === 'more' ? 'active' : ''}`}>
-        <div className="row">
-          <div className="col-12 col-xl-2 col-xxl-2"><i className="fas fa-ellipsis-h me-1"></i> </div>
-          <div className="d-none d-xl-block col-10">More</div>
-        </div>
-      </Link>
-      <Link to="/" className="list-group-item">
-        <div className="row">
-          <div className="col-12 col-xl-2 col-xxl-2"><i className="fas fa-flask"></i> </div>
-          <div className="d-none d-xl-block col-10">Labs</div>
-        </div>
-      </Link>
+      {screens.map((screen, index) =>
+        <Link to={`/tuiter/${screen}`} className={`list-group-item ${parts.at(-1) === "" || parts.at(-1) === screen ? 'active' : ''}`}>
+          <div className="row">
+            <div className="col-12 col-xl-2 col-xxl-2"><i className={`fas ${icons[index]} me-1`}></i> </div>
+            <div className="d-none d-xl-block col-10">{screen.charAt(0).toUpperCase() + screen.slice(1)}</div>
+          </div>
+        </Link>
+      )}
+
     </div>
   );
 }
