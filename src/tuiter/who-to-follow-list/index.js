5890d87c0078bf52820f559763508d150413b98a
bereket <iberekety@gmail.com>
2023-08-10 02:03:59 -0400

    Move users to server

--- src/tuiter/who-to-follow-list/index.js
+++ src/tuiter/who-to-follow-list/index.js
@@ -4,7 +4,7 @@ import WhoToFollowListItem from "./who-to-follow-list-item";
 import { useSelector } from "react-redux";
 
 const WhoToFollowList = () => {
-  const usersArray = useSelector(state => state.users);
+  const usersArray = useSelector(state => state.users.users);
   return (
     <ul className="list-group">
       <li className="list-group-item">
