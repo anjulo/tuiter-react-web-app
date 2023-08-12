0623bfc5dbf4d9ef4502f44f38a75b2d70fd6886
bereket <iberekety@gmail.com>
2023-08-12 01:38:33 -0400

    Remove unnecessary imports

--- src/tuiter/services/users-services.js
+++ src/tuiter/services/users-services.js
@@ -1,5 +1,7 @@
 import axios from "axios"
-const BASE_URL = 'http://localhost:4000'
+import dotenv from 'dotenv';
+
+const REACT_APP_API_BASE =  process.env.REACT_APP_API_BASE;
 
 const api = axios.create({
   withCredentials: true
@@ -7,22 +9,22 @@ const api = axios.create({
 
 
 export const registerUser = async (newUser) => {
-  const res = await api.post(`${BASE_URL}/register`, newUser)
+  const res = await api.post(`${REACT_APP_API_BASE}/register`, newUser)
   return res.data
 }
 
 export const logInUser = async (user) => {
-  const res = await api.post(`${BASE_URL}/login`, user)
+  const res = await api.post(`${REACT_APP_API_BASE}/login`, user)
   return res.data
 }
 
 export const logOutUser = async () => { 
-  const res = await api.post(`${BASE_URL}/logout`)
+  const res = await api.post(`${REACT_APP_API_BASE}/logout`)
   return res.data
 }
 
 export const profile = async () => {
-  const res = await api.post(`${BASE_URL}/profile`)
+  const res = await api.post(`${REACT_APP_API_BASE}/profile`)
   return res.data;
 }
 
