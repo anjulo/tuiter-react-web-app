c065b354da1393fed974681f35554555ad306e4b
bereket <iberekety@gmail.com>
2023-08-11 20:57:40 -0400

    Add cookies for cross-origin requests

--- src/tuiter/services/users-services.js
+++ src/tuiter/services/users-services.js
@@ -1,24 +1,28 @@
 import axios from "axios"
 const BASE_URL = 'http://localhost:4000'
 
+const api = axios.create({
+  withCredentials: true
+})
+
 
 export const registerUser = async (newUser) => {
-  const res = await axios.post(`${BASE_URL}/register`, newUser)
+  const res = await api.post(`${BASE_URL}/register`, newUser)
   return res.data
 }
 
 export const logInUser = async (user) => {
-  const res = await axios.post(`${BASE_URL}/login`, user)
+  const res = await api.post(`${BASE_URL}/login`, user)
   return res.data
 }
 
 export const logOutUser = async () => { 
-  const res = await axios.post(`${BASE_URL}/logout`)
+  const res = await api.post(`${BASE_URL}/logout`)
   return res.data
 }
 
 export const profile = async () => {
-  const res = await axios.get(`${BASE_URL}/profile`)
+  const res = await api.post(`${BASE_URL}/profile`)
   return res.data;
 }
 
